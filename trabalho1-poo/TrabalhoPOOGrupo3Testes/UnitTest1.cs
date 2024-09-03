507b9bfde2173012d3c6fdda868965fe6bd0d731
Renan Gomes <renango2015@gmail.com>
2024-09-02 23:14:30 -0300

    refactor: remove whitespaces

--- trabalho1-poo/TrabalhoPOOGrupo3Testes/UnitTest1.cs
+++ trabalho1-poo/TrabalhoPOOGrupo3Testes/UnitTest1.cs
@@ -1,4 +1,5 @@
 namespace TrabalhoPOOGrupo3Testes;
+
 using TrabalhoPOOGrupo3;
 
 [TestClass]
@@ -8,7 +9,6 @@ public class UnitTest1
     public void TestSum()
     {
         int sum = DesafiosGrupo3.Sum(20000, 20000);
-        
         Assert.AreEqual(40000, sum);
         Assert.AreNotEqual(40001, sum);
     }
@@ -25,7 +25,6 @@ public class UnitTest1
     public void TestMetersToMillimeters()
     {
         int millimeters = DesafiosGrupo3.MetersToMillimeters(100);
-
         Assert.AreEqual(100000, millimeters);
         Assert.AreNotEqual(100001, millimeters);
     }
@@ -43,7 +42,6 @@ public class UnitTest1
     public void TestBhaskara()
     {
         double bhaskara = DesafiosGrupo3.Bhaskara(80, 20, 40);
-
         Assert.AreEqual(3200, bhaskara);
         Assert.AreNotEqual(3200.5, bhaskara);
     }
@@ -61,9 +59,7 @@ public class UnitTest1
     public void TestRentCar()
     {
         Car car = new Car("AUDI", 2042, "AU777", 600.00);
-        
         double rent = car.CalculateRentPrice(10);
-
         Assert.AreEqual(6000.00, rent);
         Assert.AreNotEqual(6000.20, rent);
     }
@@ -72,7 +68,7 @@ public class UnitTest1
     // public void TestRentCar1()
     // {
     //     Car car = new Car("AUDI", 2042, "AU777", 600.00);
-        
+
     //     double rent = car.CalculateRentPrice(10);
 
     //     // Dar errado
